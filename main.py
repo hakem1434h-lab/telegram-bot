b141b62548c09bbc2b9e1a88661c0d7cc5be08d0
hakem1434h-lab <hakem1434h@gmail.com>
2026-07-03 05:57:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,6 +50,6 @@ def home():
 def run_bot():
     bot.infinity_polling()
 
-if name == '__main__':
+if __name__ == '__main__':
     threading.Thread(target=run_bot, daemon=True).start()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
